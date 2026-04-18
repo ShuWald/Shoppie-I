0361e472864a924de6ce6d42156f701a08d9ab78
ShuWald <shubhambirendarwaldiya12345@gmail.com>
2026-04-18 08:42:31 -0700

    something

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,25 @@
 from typing import Annotated
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
+app = FastAPI()
+
+# initializes middleware to allow requests into endpoints
+def initialize():
+    # Probably initialize all routes + handlers in here
+    # Will also need function for posting requests
+    pass
+
+@app.get("/test")
+def test():
+    return {"message": "test message"}
+
+# tests all routing/related functionality 
+def testing():
+    pass
+
+
+# This is the entry point for the code/application
+if __name__ == "__main__":
+    initialize()
